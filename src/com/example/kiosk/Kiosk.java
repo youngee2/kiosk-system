e1682829e371d699f77d0313ca29e111eb4e40bd
youngee2 <jr3537@naver.com>
2025-04-24 15:16:52 +0900

    feat: 유효하지 않은 입력에 대해 오류 메시지 출력 구현

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -1,6 +1,7 @@
 package com.example.kiosk;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,7 +10,7 @@ import static com.example.kiosk.Main.isOnOff;
 //프로그램 순서 및 흐름 제어를 담당하는 클래스
 public class Kiosk {
     static final List<MenuItem> menuList = new ArrayList<>();
-
+    int orderMenu;
 
     //클래스 생성자를 통해 값을 할당
     Kiosk(String itemName, double itemPrice, String itemInfo){
@@ -24,24 +25,38 @@ public class Kiosk {
     //등록된 메뉴 출력
         public void start() {
             Scanner sc = new Scanner(System.in);
-            do {
-            System.out.println("[ SHAKESHACK MENU ]");
-            for (int i = 0; i <= menuList.size() - 1; i++) {
-                MenuItem menuItem = menuList.get(i);
-                System.out.println((i + 1) + ". " + menuItem.getItemName() + " | W " + menuItem.getItempPrice() + " | " + menuItem.getItemInfo());
-                if (i == menuList.size() - 1) {
-                    System.out.println("0. 종료  | 종료");
+
+            while(isOnOff) {
+                System.out.println("[ SHAKESHACK MENU ]");
+                for (int i = 0; i <= menuList.size() - 1; i++) {
+                    MenuItem menuItem = menuList.get(i);
+                    System.out.println((i + 1) + ". " + menuItem.getItemName() + " | W " + menuItem.getItempPrice() + " | " + menuItem.getItemInfo());
+                    if (i == menuList.size() - 1) {
+                        System.out.println("0. 뒤로가기  | 뒤로가기");
+                    }
                 }
-            }
-                int orderMenu = sc.nextInt();
-                menuSelect(orderMenu);
-                //키오스크 프로그램 종료
-                if(orderMenu==0){
-                    isOnOff=false;
-                    System.out.println("프로그램을 종료합니다.");
+                //nextInt()를 사용하면 잘못된 입력이 버퍼에 남아 있어서 무한 반복
+                String input=sc.next();
+                try {
+                    int orderMenu = Integer.parseInt(input);
+                    //숫자인데 메뉴에 없는 숫자를 선택했을 때 출력.
+                    if (orderMenu>menuList.size()) {
+                        //메뉴 갯수보다 같거나 적으면서 0이 아니면 해당되는 메뉴 로직 실행.
+                        System.out.println("존재하지 않는 메뉴입니다.");
+
+                    } else if(orderMenu!=0){
+                        menuSelect(orderMenu);
+                        //0을 입력하면 프로그램이 뒤로가게 작성
+                    
+                    }else{
+                        System.out.println("처음으로 돌아갑니다. 메뉴를 선택해주세요.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("숫자만 입력해주세요.");
                 }
 
-            }while (isOnOff);
+
+            }
 
         }
 
