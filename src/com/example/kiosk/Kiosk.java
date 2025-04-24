e701b3e2721c148e484ad821444cf93b26994415
youngee2 <jr3537@naver.com>
2025-04-24 14:03:58 +0900

    feat: 키오스크 메뉴 데이터 관리 로직 구현

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -7,16 +7,16 @@ public class Kiosk {
 
         switch (menuNumber){
             case 1:
-                System.out.println("shackBurger | W6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                System.out.println("선택한 메뉴: shackBurger | W6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
             break;
             case 2:
-                System.out.println("SmokeShack | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                System.out.println("선택한 메뉴: SmokeShack | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
             break;
             case 3:
-                System.out.println("Cheeseburger | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                System.out.println("선택한 메뉴: Cheeseburger | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
             break;
             case 4:
-                System.out.println("Hamburger | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                System.out.println("선택한 메뉴: Hamburger | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
             case 0:
                 break;
         }
--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -4,5 +4,45 @@ package com.example.kiosk;
 //햄버거의 이름, 가격 설명
 // ex)ShackBurger, 6.9, 토마토, 양상추, 쉑소스가 토핑된 치즈버거
 // ex) new MenuItem("ShackBurger",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거")
+
+
 public class MenuItem {
+    //속성
+    private String itemName; //메뉴 이름
+    private double itempPrice; //메뉴 가격
+    private String itemInfo; //메뉴 설명
+
+public MenuItem(String itemName, double itemPrice, String itemInfo){
+    this.itemName=itemName;
+    this.itempPrice=itemPrice;
+    this.itemInfo=itemInfo;
+    }
+
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getItempPrice() {
+        return itempPrice;
+    }
+
+    public void setItempPrice(double itempPrice) {
+        this.itempPrice = itempPrice;
+    }
+
+    public String getItemInfo() {
+        return itemInfo;
+    }
+
+    public void setItemInfo(String itemInfo) {
+        this.itemInfo = itemInfo;
+    }
+
+
+
 }
